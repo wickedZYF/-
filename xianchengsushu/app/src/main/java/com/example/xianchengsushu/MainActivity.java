401fe5097dca0373869f04171ce59a4b85f3f6cb
wickedZYF <1002835548@qq.com>
2020-09-23 20:14:26 +0800

    线程判断素数完全体

--- xianchengsushu/app/src/main/java/com/example/xianchengsushu/MainActivity.java
+++ xianchengsushu/app/src/main/java/com/example/xianchengsushu/MainActivity.java
@@ -3,25 +3,39 @@ package com.example.xianchengsushu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
  EditText num;
-
+Button button;
+TextView jieguo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        num = findViewById(R.id.num);
+         jieguo = findViewById(R.id.jieguo);
+        final Handler handler = new Handler(){
+                @Override
+            public void handleMessage(Message msg) {
+                jieguo.setText((String)msg.obj);
+            }
+        };
+
         final Runnable myWorker = new Runnable() {
             @Override
             public void run() {
-                String s = num.getText().toString().trim();
+                String s = num.getText().toString();
                 double b = Double.valueOf(s);
                 boolean c = true;
+                Message msg= new Message();
                 if (b==1){
                     c = false;
                 }
@@ -35,16 +49,17 @@ public class MainActivity extends AppCompatActivity {
                 }
                 if( c == true)
                 {
-                    Toast.makeText(MainActivity.this, "是素数", Toast.LENGTH_LONG).show();
+                   msg.obj="是素数";
 
                 }
                 else
                 {
-                    Toast.makeText(MainActivity.this, "不是素数", Toast.LENGTH_LONG).show();
+                    msg.obj="不是素数";
                 }
+                handler.sendMessage(msg);
             }
         };
-        Button button = findViewById(R.id.button);
+         button = findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
 
             @Override
